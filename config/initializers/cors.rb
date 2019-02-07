de4dc5397e25b556ca8641e7b3a19d88850a81cd
Alexander Fitterling <info@sp33c.de>
2019-02-07 17:42:26 +0800

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,10 +17,12 @@
 
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'localhost:4200'
-      resource '*',
-        headers: :any,
-        methods: %i(get post put patch delete options head)
-    end
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :options]
+  
   end
+end
