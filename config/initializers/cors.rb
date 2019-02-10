385bbece78cacc64f4456052cf2c6633415ea2be
Alexander Fitterling <info@sp33c.de>
2019-02-10 20:32:35 +0800

    cors dev env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,14 +15,20 @@
 #   end
 # end
 
-# config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:4200'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :options],
-      credentials: true
+# config/initializers/cors.rb
+
+if Rails.env.development?
+
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:4200'
+
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :options],
+        credentials: true
+    end
   end
+
 end
