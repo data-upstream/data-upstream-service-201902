8f7a24fb0be7c14307a01158eb6e5a4f3ff10e0f
Alexander Fitterling <info@sp33c.de>
2019-02-10 20:05:48 +0800

    credential

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,11 +18,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:4200'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :options]
-  
+      methods: [:get, :post, :options],
+      credentials: true
   end
 end
